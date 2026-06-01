74f82f2e9d1525e58702689a82ae9b7e8df5b0c9
IT24103798 <it24103798@my.sliit.lk>
2026-06-01 22:25:08 +0530

    Add social login and password reset functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password-otp" element={<ResetPasswordOtp />} />
+          
 
           <Route
             path="/my-orders"
